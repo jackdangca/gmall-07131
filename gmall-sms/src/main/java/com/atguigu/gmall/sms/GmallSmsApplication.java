faa8b3b88b5fa134905f3f002b713cdf08e87bef
yuanjinminggit <1779569744@qq.com>
2021-02-19 20:39:13 +0800

    创建项目

---
+++ gmall-sms/src/main/java/com/atguigu/gmall/sms/GmallSmsApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.sms;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@MapperScan("com.atguigu.gmall.sms.mapper")
+@EnableFeignClients
+@SpringBootApplication
+public class GmallSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallSmsApplication.class, args);
+    }
+
+}
